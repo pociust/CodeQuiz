2506190456685df9d91fb81ac2dd0dae9e86e7eb
Tom Pocius <poci14@gmail.com>
2019-10-07 21:55:45 -0500

    created buttons in html, linked js for buttos to cylce through and select right/wrong answer in console and move to next question

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ let minutesDisplay = document.getElementById('minutes');
 let  secondsDisplay = document.getElementById('seconds');
 let startButton = document.getElementById('start-game');
 let timer = 90;
+let activeQuestion = 0;
+let choiceA = document.getElementById('choice-a');
+let choiceB = document.getElementById('choice-b');
+let choiceC = document.getElementById('choice-c');
+let choiceD = document.getElementById('choice-d');
+let theQuestion = document.getElementById('the-question');
+let newGame = document.getElementById('questions');
 const questions = [
   {
     title: "1Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    answer: "strings"
   },
   {
     title: "2Commonly used data types DO NOT include:",
@@ -22,23 +29,47 @@ const questions = [
   {
     title: "4Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    answer: "string"
   },
   {
     title: "5Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    answer: "number"
   },
 ];
 
-let newGame = document.getElementById('questions');
+function swapQuestion() {
+  activeQuestion = activeQuestion + 1;
+  theQuestion.innerText = `${activeQuestion + 1}. ${questions[activeQuestion].title}`;
+  choiceA.innerText = `A: ${questions[activeQuestion].choices[0]}`;
+  choiceB.innerText = `B: ${questions[activeQuestion].choices[1]}`;
+  choiceC.innerText = `C: ${questions[activeQuestion].choices[2]}`;
+  choiceD.innerText = `D: ${questions[activeQuestion].choices[3]}`;
+};
+
+function checkAnswer(event) {
+  if (event.target.innerText.substr(3) === questions[activeQuestion].answer) {
+    console.log('you got it partner!')
+    
+  } else {
+    console.log('you suck')
+  }
+  swapQuestion()
+};
+choiceA.addEventListener('click', function(){ checkAnswer(event)});
+choiceB.addEventListener('click', function(){ checkAnswer(event)});
+choiceC.addEventListener('click', function(){ checkAnswer(event)});
+choiceD.addEventListener('click', function(){ checkAnswer(event)});
+
+
 
 
 startButton.addEventListener('click', function playGame(){
   startTimer();
   newGame.hidden = false;
   startButton.hidden = true;
-  let activeQuestion = 0;
+  activeQuestion = -1;
+  swapQuestion();
 
 
 
@@ -49,7 +80,6 @@ startButton.addEventListener('click', function playGame(){
 
 function startTimer() {
   minutes = parseInt(timer / 60);
-  console.log(minutes);
   seconds = parseInt (timer % 60);
 
   minutesDisplay.innerText = '0' + minutes +':'; 
@@ -64,8 +94,6 @@ function startTimer() {
   setTimeout(function() {
     startTimer();
   }, 1000);
-
-
 
 
   // if (timer === 0) {
