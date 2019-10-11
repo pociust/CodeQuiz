ed12ec4069c93e0ead560e3bf1a9e2ed814e2a3d
Tom Pocius <poci14@gmail.com>
2019-10-10 23:49:06 -0500

    fiexd some js to clean up the hidding elemenets, rearranged html for button next to eachother, renamed some css to decrease classes

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
 // set the variables
-
+const questions = [
+  {
+    title: `Which famous person was the first to have a star laid on the "Hollywood Walk of
+    Fame"?:`,
+    choices: [`Elizabeth Taylor`, `Charlie Chaplin`, `Stanley Kramer`, `Burt Lancaster`],
+    answer: `Stanley Kramer`
+  },
+  {
+    title: `"Katniss Everdeen" takes her sister's place in the "Hunger Games". Which district
+    does she represent?:`,
+    choices: [`12`, `6`, `3`, `9`],
+    answer: `12`
+  },
+  {
+    title: `In Greek mythology, who is the god of the winds and king of the mythical, floating
+    island of Aiolia?`,
+    choices: [`Crios`, `Kratos`, `Aeolus`, `Dinlas`],
+    answer: `Aeolus`
+  },
+  {
+    title: `What British monarch reigned for only nine days?:`,
+    choices: [`George the II`, `Edward the V`, `Mari I`, `Lady Jane Grey`],
+    answer: `Lady Jane Grey`
+  },
+  {
+    title: ` Which planet in our solar system spins the fastest?:`,
+    choices: [`Earth`, `Jupiter`, `Neptune`, `Mercury`],
+    answer: `Jupiter`
+  },
+  {
+    title: `How many bones are in the adult human body?:`,
+    choices: [`6`, `195`, `83`, `206`],
+    answer: `206`
+  },
+  {
+    title: `Which famous person was the first to have a star laid on the "Hollywood Walk of
+    Fame"?:`,
+    choices: [`Elizabeth Taylor`, `Charlie Chaplin`, `Stanley Kramer`, `Burt Lancaster`],
+    answer: `Stanley Kramer`
+  },
+  {
+    title: `Which famous person was the first to have a star laid on the "Hollywood Walk of
+    Fame"?:`,
+    choices: [`Elizabeth Taylor`, `Charlie Chaplin`, `Stanley Kramer`, `Burt Lancaster`],
+    answer: `Stanley Kramer`
+  },
+  {
+    title: `Which famous person was the first to have a star laid on the "Hollywood Walk of
+    Fame"?:`,
+    choices: [`Elizabeth Taylor`, `Charlie Chaplin`, `Stanley Kramer`, `Burt Lancaster`],
+    answer: `Stanley Kramer`
+  },
+];
 let minutesDisplay = document.getElementById('minutes');
 let  secondsDisplay = document.getElementById('seconds');
-let startButton = document.getElementById('start-game');
 let timer = 90;
 let activeQuestion = 0;
 let choiceA = document.getElementById('choice-a');
@@ -10,78 +61,31 @@ let choiceB = document.getElementById('choice-b');
 let choiceC = document.getElementById('choice-c');
 let choiceD = document.getElementById('choice-d');
 let theQuestion = document.getElementById('the-question');
-let newGame = document.getElementById('questions');
-let highScore = document.getElementById('high-score');
-let clock = document.getElementById('start-timer');
+let userScore = document.getElementById('user-score');
+let finalQuestion = questions.length - 1;
+let score = 0;
 // making the questions
 
-const questions = [
-  {
-    title: "1Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "strings"
-  },
-  {
-    title: "2Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "3Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "4Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "string"
-  },
-  {
-    title: "5Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "number"
-  },
-  {
-    title: "2Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "3Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "4Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "string"
-  },
-  {
-    title: "5Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "number"
-  },
-  {
-    title: "2Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "3Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "4Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "string"
-  },
-  {
-    title: "5Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "number"
-  },
-];
+
+function toggleHidden(className) {
+  var elements = document.getElementsByClassName(className);
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].hidden = !elements[i].hidden;
+  }
+};
+
+function showScore (){
+  toggleHidden('stage-1');
+  toggleHidden('stage-2');
+  toggleHidden('stage-3');
+  userScore.innerText = `Final Score: ${score}`;
+  let newScore = {name: 'Tom', score: score};
+  let previousScore = JSON.parse(localStorage.getItem('scoreboard'));
+  previousScore = previousScore.concat(newScore);
+  console.log(previousScore);
+  localStorage.setItem("scoreboard", JSON.stringify(previousScore));
+};
+
 
 function swapQuestion() {
   activeQuestion = activeQuestion + 1;
@@ -91,15 +95,20 @@ function swapQuestion() {
   choiceC.innerText = `C: ${questions[activeQuestion].choices[2]}`;
   choiceD.innerText = `D: ${questions[activeQuestion].choices[3]}`;
 };
-let score = 0;
+
 function checkAnswer(event) {
   if (event.target.innerText.substr(3) === questions[activeQuestion].answer) {
-    score++;
+    score = score + 1;
     
   } else {
-    score--;
+    score = score - 5;
   }
-  swapQuestion()
+  event.target.blur();
+  if (activeQuestion < finalQuestion) {
+    swapQuestion();
+  } else {
+    showScore();
+};
 };
 
 choiceA.addEventListener('click', function(){ checkAnswer(event)});
@@ -133,39 +142,13 @@ function startTimer() {
 
 };
 
-startButton.addEventListener('click', function playGame(){
+
+document.getElementById('start-game').addEventListener('click', function playGame(){
   startTimer();
-  newGame.hidden = false;
-  startButton.hidden = true;
+  toggleHidden('stage-2');
+  toggleHidden('stage-1');
   activeQuestion = -1;
   swapQuestion();
 });
 
-//high score button
-highScore.addEventListener('click', function scoreBoard (){
-  newGame.hidden = true;
-  startButton.hidden = true;
-  clock.hidden = true;
-  highScore.hidden = true;
 
-
-
-});
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
