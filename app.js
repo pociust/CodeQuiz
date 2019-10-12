5d55b3358a62f7f94dc4207c271187a464efa5d6
Tom Pocius <poci14@gmail.com>
2019-10-12 16:54:50 -0500

    fixed some aspect of the timer, got timer to be added into score

--- app.js
+++ app.js
@@ -64,62 +64,16 @@ let theQuestion = document.getElementById('the-question');
 let userScore = document.getElementById('user-score');
 let finalQuestion = questions.length - 1;
 let score = 0;
+let totalScore = 0;
 // making the questions
 
-
-function toggleHidden(className) {
-  var elements = document.getElementsByClassName(className);
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].hidden = !elements[i].hidden;
-  }
-};
-
-function showScore (){
-  toggleHidden('stage-1');
-  toggleHidden('stage-2');
-  toggleHidden('stage-3');
-  userScore.innerText = `Final Score: ${score}`;
-  let newScore = {name: 'Tom', score: score};
-  let previousScore = JSON.parse(localStorage.getItem('scoreboard'));
-  previousScore = previousScore.concat(newScore);
-  console.log(previousScore);
-  localStorage.setItem("scoreboard", JSON.stringify(previousScore));
-};
-
-
-function swapQuestion() {
-  activeQuestion = activeQuestion + 1;
-  theQuestion.innerText = `${activeQuestion + 1}. ${questions[activeQuestion].title}`;
-  choiceA.innerText = `A: ${questions[activeQuestion].choices[0]}`;
-  choiceB.innerText = `B: ${questions[activeQuestion].choices[1]}`;
-  choiceC.innerText = `C: ${questions[activeQuestion].choices[2]}`;
-  choiceD.innerText = `D: ${questions[activeQuestion].choices[3]}`;
-};
-
-function checkAnswer(event) {
-  if (event.target.innerText.substr(3) === questions[activeQuestion].answer) {
-    score = score + 1;
-    
-  } else {
-    score = score - 5;
-  }
-  event.target.blur();
-  if (activeQuestion < finalQuestion) {
-    swapQuestion();
-  } else {
-    showScore();
-};
-};
-
-choiceA.addEventListener('click', function(){ checkAnswer(event)});
-choiceB.addEventListener('click', function(){ checkAnswer(event)});
-choiceC.addEventListener('click', function(){ checkAnswer(event)});
-choiceD.addEventListener('click', function(){ checkAnswer(event)});
-
-
-// making the timer
-
 function startTimer() {
+
+ let newTimeOut = setInterval(
+   
+  
+  function () {
+
   minutes = parseInt(timer / 60);
   seconds = parseInt (timer % 60);
 
@@ -132,18 +86,85 @@ function startTimer() {
   secondsDisplay.innerText = seconds;
   timer --;
 
-  setTimeout(function() {
-    startTimer();
-  }, 1000);
-   // if (timer === 0) {
-  //   alert('game over man!');
-  //   timer = 90;
-  // }
+  if (timer < 0 || activeQuestion > finalQuestion) {
+    clearInterval(newTimeOut);
+    showScore();
+    
+  }
+  // console.log(timer)
+ }, 1000);
+ 
+   
 
 };
 
+function toggleHidden(className) {
+  var elements = document.getElementsByClassName(className);
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].hidden = !elements[i].hidden;
+  }
+};
+
+function showScore (){
+  toggleHidden('stage-1');
+  toggleHidden('stage-2');
+  toggleHidden('stage-3');
+
+  console.log(timer);
+
+  userScore.innerText = `Final Score: ${totalScore}`;
+  let newScore = {name: 'Tom', totalScore: totalScore};
+  let previousScore = JSON.parse(localStorage.getItem('scoreboard'));
+  if (previousScore) {
+    previousScore = previousScore.concat(newScore);
+  } else {
+    previousScore = [newScore];
+  }
+  localStorage.setItem("scoreboard", JSON.stringify(previousScore));
+};
+
+
+
+function swapQuestion() {
+  activeQuestion = activeQuestion + 1;
+  theQuestion.innerText = `${activeQuestion + 1}. ${questions[activeQuestion].title}`;
+  choiceA.innerText = `A: ${questions[activeQuestion].choices[0]}`;
+  choiceB.innerText = `B: ${questions[activeQuestion].choices[1]}`;
+  choiceC.innerText = `C: ${questions[activeQuestion].choices[2]}`;
+  choiceD.innerText = `D: ${questions[activeQuestion].choices[3]}`;
+};
+
+function checkAnswer(event) {
+  
+  if (event.target.innerText.substr(3) === questions[activeQuestion].answer) {
+    score = score + 1;
+    
+  } else {
+    score = score - 5;
+  }
+  if (activeQuestion < finalQuestion) {
+    swapQuestion();
+  } else {
+    totalScore = score + timer;
+    showScore();
+};
+event.target.blur();
+
+};
+
+choiceA.addEventListener('click', function(){ checkAnswer(event)});
+choiceB.addEventListener('click', function(){ checkAnswer(event)});
+choiceC.addEventListener('click', function(){ checkAnswer(event)});
+choiceD.addEventListener('click', function(){ checkAnswer(event)});
+
+
+// making the timer
+
+
+
 
 document.getElementById('start-game').addEventListener('click', function playGame(){
+  timer = 90;
   startTimer();
   toggleHidden('stage-2');
   toggleHidden('stage-1');
@@ -151,4 +172,6 @@ document.getElementById('start-game').addEventListener('click', function playGam
   swapQuestion();
 });
 
-
+// document.getElementById('high-score').addEventListener('click' function () {
+  // toggleHidden()
+// })
